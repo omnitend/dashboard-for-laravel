df78ab5b2fc24cfe299cdbfbec48e8a67fa43df1
James Pickard <james@omnitend.com>
2025-11-16 09:05:00 +0000

    Fix router.reload() - remove invalid preserveState option

--- resources/js/components/extended/DXTable.vue
+++ resources/js/components/extended/DXTable.vue
@@ -816,7 +816,7 @@ const refresh = () => {
     }
     // Inertia mode: reload current page to refresh data
     else if (isInertiaMode.value && props.inertiaUrl) {
-        router.reload({ preserveState: true, preserveScroll: true });
+        router.reload({ preserveScroll: true });
     }
 };
 
